365463a089f40205552569c53ad73d4739ae9ae2
crdev <CristianOssesSaavedra1989@gmail.com>
2026-04-22 13:59:44 -0400

    Corrección de código

--- ProyectoFundamentosCs.py
+++ ProyectoFundamentosCs.py
@@ -1,20 +1,16 @@
 import sys
 
 def principal():
-    # Bucle infinito hasta encontrar la condición de quiebre
     while True:
         try:
-            # Leemos la primera línea del bloque
             linea1 = input().strip()
         except EOFError:
-            # Si se acaba el archivo de entrada repentinamente
             break 
             
         # Condición de salida del programa
         if linea1 == "FIN":
             break
             
-        # Si no es FIN, leemos las otras 5 líneas correspondientes a este autómata
         try:
             linea2 = input().strip()
             linea3 = input().strip()
@@ -23,11 +19,10 @@ def principal():
             linea6 = input().strip()
         except EOFError:
             break
-
-        # Empaquetamos las líneas en una lista para pasarlas a la función validadora
+        
         lineas_automata = [linea1, linea2, linea3, linea4, linea5, linea6]
         
-        # Llamamos a la función que procesará este bloque en particular
+        
         procesar_automata(lineas_automata)
 
 
@@ -37,7 +32,6 @@ def procesar_automata(lineas):
     Si todo está correcto, procederá a la computación.
     Si hay error, imprimirá "Error encontrado en [X]".
     """
-    # Aquí irá toda nuestra lógica de validación (Fase 2)
     pass
 
 
